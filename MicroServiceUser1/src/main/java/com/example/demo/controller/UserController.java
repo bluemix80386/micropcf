5e505603053386544f7d5f9765d056926af96278
bluemix80386 <40419376+bluemix80386@users.noreply.github.com>
2018-06-20 14:19:11 +0530

    Update UserController.java

--- MicroServiceUser1/src/main/java/com/example/demo/controller/UserController.java
+++ MicroServiceUser1/src/main/java/com/example/demo/controller/UserController.java
@@ -34,7 +34,7 @@ public class UserController {
 	
 	@RequestMapping("/HELLO")
 	private String helloOne(){
-		return "Hi welcome to PCF........ ";
+		return "Hi welcome to PCF........ Good";
 	}
 
 	
