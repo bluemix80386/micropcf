c2ede25881ec2bd93147aa0dfd44ccd88f1dbb70
bluemix80386 <40419376+bluemix80386@users.noreply.github.com>
2018-06-20 15:09:33 +0530

    Update MicroServiceUser1Application.java

--- MicroServiceUser1/src/main/java/com/example/demo/MicroServiceUser1Application.java
+++ MicroServiceUser1/src/main/java/com/example/demo/MicroServiceUser1Application.java
@@ -9,6 +9,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 public class MicroServiceUser1Application {
 
 	public static void main(String[] args) {
+		
 		SpringApplication.run(MicroServiceUser1Application.class, args);
 	}
 }
